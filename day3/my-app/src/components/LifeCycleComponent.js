11e2af05f99f8c91eed371db78b6d1ca2e38c4ea
刘春华 <chunhua.liu@91power.com>
2019-07-25 11:54:19 +0800

    code

--- day3/my-app/src/components/LifeCycleComponent.js
+++ day3/my-app/src/components/LifeCycleComponent.js
@@ -1,45 +1,35 @@
 import React  from 'react';
-
 export default class LifeCycleComponent extends React.Component {
-
     constructor(props) {
         super(props);
-
+        debugger;
         this.divRef = React.createRef();
-
-
-        this.state = {
-            count: 0
-        };
-
+        this.state = {count: 0};
         console.log('constructor 构造函数被调用');
         alert('constructor 构造函数被调用');
     }
-
-    static getDerivedStateFromProps(nextProps, prevState) {
-
+    static getDerivedStateFromProps(nextProps, prevState) { // 获取到外部参数是 调用, 来确定下一个数据状态
+        debugger;
         console.log('getDerivedStateFromProps, nextProps = ', nextProps);
         console.log('getDerivedStateFromProps, prevState = ', prevState);
         alert('getDerivedStateFromProps');
-        return prevState;
+        return prevState;  // 返回下一步的数据状态
     }
-
     addCount() {
+        debugger;
         this.setState({
             count: this.state.count + 1
         })
     }
-
-    shouldComponentUpdate(nextProps, nextState) {
+    shouldComponentUpdate(nextProps, nextState) { // 控制 是否需要 渲染 当前组件
+        debugger;
         console.log('shouldComponentUpdate  nextProps = ', nextProps);
         console.log('shouldComponentUpdate nextState = ', nextState);
-
         console.log('shouldComponentUpdate  props = ', this.props);
-
-        return nextProps.name !== this.props.name || nextProps.age !== this.props.age;
+        return nextProps.name !== this.props.name || nextProps.age !== this.props.age || nextState.count !== this.state.count
     }
-
     render() {
+        debugger;
         console.log('render 渲染被调用');
         alert('render 渲染被调用');
         return (
@@ -50,27 +40,27 @@ export default class LifeCycleComponent extends React.Component {
             </div>
         )
     }
-
-    getSnapshotBeforeUpdate(prevProps, prevState) {
+    getSnapshotBeforeUpdate(prevProps, prevState) { // 真正渲染UI前给程序猿 一个机会去获取当前ui 的 dom 状态
+        debugger;
         console.log("getSnapshotBeforeUpdate  prevProps = ", prevProps);
         console.log("getSnapshotBeforeUpdate  prevState = ", prevState);
         alert(this.divRef.current.innerHTML);
 
         return this.divRef.current.offsetWidth;
     }
-
-    componentDidUpdate(prevProps, prevState, snapshot){
+    componentDidUpdate(prevProps, prevState, snapshot){ // 当数据模型修改介绍后 调用
+        debugger;
         console.log('componentDidUpdate prevProps = ', prevProps);
         console.log('componentDidUpdate prevState = ', prevState);
         console.log(snapshot);
     }
-
-    componentDidMount() {
+    componentDidMount() { // 当组件挂载到 父组件的时候调用
+        debugger;
         console.log('componentDidMount, 组件已经被挂载');
         alert('componentDidMount, 组件已经被挂载');
     }
-
-    componentWillUnmount() {
+    componentWillUnmount() {  // 当组件卸载的时候调用
+        debugger;
         console.log('componentWillUnmount');
         alert('componentWillUnmount')
     }
---
+++ day4/my-app/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+// 结构出 Provider 组件 用来提供上下文数据
+
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+  }
+  render() {
+
+    return (
+        <div>
+          Redux
+        </div>
+    )
+  }
+}
+
+export default App;
